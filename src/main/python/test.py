d2e7cd4e3e323b545d1b5577b58974b00df3701c
tigeu <tigeu100@hhu.de>
2023-01-04 16:45:15 +0100

    Split query into two queries to prevent counting reviews from the test set

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -29,43 +29,46 @@ PREFIX itsrdf:<https://www.w3.org/2005/11/its/rdf#>
 PREFIX schema:<http://schema.org/>
 PREFIX dbr:<http://dbpedia.org/resource/>
 
-SELECT ?claim ?text ?authoredCountFalse ?authoredCountTrue ?authoredCountOther ?mentions ?citations
+SELECT ?claim ?text ?author ?mentions ?citations
 WHERE {{
     ?claim a schema:CreativeWork ; 
            schema:text ?text
     # only use current claim
     FILTER(STR(?claim)="{0}")
+    OPTIONAL{{?claim schema:author ?author}}
     # count mentions
-    {{
-        OPTIONAL{{
-            SELECT ?claim (COUNT(?mention) AS ?mentions) WHERE {{
-                ?claim schema:mentions ?mention
-            }} GROUP BY ?claim
-        }}
-    }}
-    # count citations
-    {{
-        OPTIONAL{{
-            SELECT ?claim (COUNT(?citation) AS ?citations) WHERE {{
-                ?claim schema:citation ?citation
-            }} GROUP BY ?claim
-        }}
-    }}
-    ?claim schema:author ?author .
-    # count authored false/true/other
-    {{
-        SELECT ?claim (SUM(?authoredCountFalse) AS ?authoredCountFalse) (SUM(?authoredCountTrue) AS ?authoredCountTrue) (SUM(?authoredCountOther) AS ?authoredCountOther) WHERE {{
-            ?claim schema:author/^schema:author ?authoredClaims .
-            # make sure current claim is not counted
-            FILTER(STR(?authoredClaims)!=STR(?claim))
-            # make sure other claims in test set are not counted
-            #FILTER(STR(?authoredClaims) not in ({0}))
-            ?authoredClaims ^schema:itemReviewed/schema:reviewRating ?reviewRating .
-            BIND(IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_FALSE", 1, 0) AS ?authoredCountFalse)
-            BIND(IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_TRUE", 1, 0) AS ?authoredCountTrue)
-            BIND(IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_OTHER", 1, 0) AS ?authoredCountOther)
+    OPTIONAL{{
+        SELECT ?claim (COUNT(?mention) AS ?mentions) WHERE {{
+            ?claim schema:mentions ?mention
         }} GROUP BY ?claim
     }}
+    # count citations
+    OPTIONAL{{
+        SELECT ?claim (COUNT(?citation) AS ?citations) WHERE {{
+            ?claim schema:citation ?citation
+        }} GROUP BY ?claim
+    }}
+}}
+"""
+
+author_query = """
+PREFIX itsrdf:<https://www.w3.org/2005/11/its/rdf#>
+PREFIX schema:<http://schema.org/>
+PREFIX dbr:<http://dbpedia.org/resource/>
+
+SELECT ?claim ?groundTruth
+WHERE {{
+    ?claim a schema:CreativeWork ; 
+           schema:author ?author 
+    FILTER(STR(?author)="{0}")
+    # only use claims that have a FALSE, TRUE, or OTHER review
+    ?claim ^schema:itemReviewed ?review .
+    ?review schema:reviewRating ?reviewRating
+    FILTER(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_TRUE" || STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_FALSE" || STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_OTHER")
+    # bind FALSE to 0, TRUE to 1, OTHER to 2
+    BIND(IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_FALSE", 0, 
+        IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_TRUE", 1, 
+            IF(STR(?reviewRating)="http://data.gesis.org/claimskg/rating/normalized/claimskg_OTHER", 2, -1))) AS ?groundTruth)
 }}
 """
 
@@ -73,11 +76,56 @@ print("Executing test query")
 X_test = []
 claim_data = []
 for index, claim in enumerate(claims):
-    print(f"Processing {index}/{claim_amount}")
+    print(f"Processing {index+1}/{claim_amount}")
     formatted_query = query.format(claim)
     sparql.setQuery(formatted_query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
+
+    # get author of current query
+    author = ""
+    for result in results['results']['bindings']:
+        if 'author' in result:
+            author = result['author']['value']
+
+    if author:
+        formatted_author_query = author_query.format(author)
+        sparql.setQuery(formatted_author_query)
+        sparql.setReturnFormat(JSON)
+        author_results = sparql.query().convert()
+
+        # count false, true and other claims
+        authoredCountFalse = 0
+        authoredCountTrue = 0
+        authoredCountOther = 0
+        for result in author_results['results']['bindings']:
+            current_claim = result['claim']['value']
+            # don't count claims from test set
+            if current_claim in claims:
+                continue
+
+            if 'groundTruth' in result:
+                ground_truth = int(result['groundTruth']['value'])
+                if ground_truth == 0:
+                    authoredCountFalse += 1
+                elif ground_truth == 1:
+                    authoredCountTrue += 1
+                elif ground_truth == 2:
+                    authoredCountOther += 1
+
+        if len(results['results']['bindings']) == 0:
+            print(claim)
+
+    else:
+        authoredCountFalse = 0
+        authoredCountTrue = 0
+        authoredCountOther = 0
+
+    # add to results
+    for result in results['results']['bindings']:
+        result['authoredCountFalse'] = {'value': authoredCountFalse}
+        result['authoredCountTrue'] = {'value': authoredCountTrue}
+        result['authoredCountOther'] = {'value': authoredCountOther}
 
     X, data = parse_results(results, test=True)
 
